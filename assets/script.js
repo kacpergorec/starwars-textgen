253eb41449a790e1a5816c2c0fa3d9beff72697f
Kacper <kacper@mediart.pl>
2021-06-16 21:05:22 +0200

    -Form with basic logic -Stylized input fields

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $('.play').click(function(){
+        $(".form-wrapper").addClass("playing");
+        $(".scroll").addClass("playing");
+        $(".logo").addClass("playing");
+    });
+});
